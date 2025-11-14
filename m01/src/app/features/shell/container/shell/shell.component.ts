19156332adb8381d87e6eeb45a597f925169036f
FabianGosebrink <fabian.gosebrink@outlook.com>
2025-11-14 19:54:13 +0100

    Refactor ShellComponent by removing inline template and fixing import statement

--- m01/src/app/features/shell/container/shell/shell.component.ts
+++ m01/src/app/features/shell/container/shell/shell.component.ts
@@ -1,18 +1,12 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { CheckoutService } from '../../../../shared/services/checkout.service';
 import { HeaderComponent } from '../../presentational/header/header.component';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-shell',
   imports: [HeaderComponent, RouterOutlet],
-  template: `
-    <app-header class="header" [cartProductsCount]="cartProductsCount()" />
-
-    <div class="content">
-      <router-outlet />
-    </div>
-  `,
+  templateUrl: './shell.component.html',
   styleUrl: './shell.component.scss',
 })
 export class ShellComponent implements OnInit {
