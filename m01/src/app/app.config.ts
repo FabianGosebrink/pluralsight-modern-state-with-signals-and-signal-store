75ef8f95904fda22d6483cae5cac881613e72e67
FabianGosebrink <fabian.gosebrink@outlook.com>
2025-11-26 21:11:31 +0100

    Refactor app configuration: remove unused NgRx imports and streamline providers setup

--- m01/src/app/app.config.ts
+++ m01/src/app/app.config.ts
@@ -1,15 +1,8 @@
-import {
-  ApplicationConfig,
-  isDevMode,
-  provideZoneChangeDetection,
-} from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideToastr } from 'ngx-toastr';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-import { provideStore } from '@ngrx/store';
-import { provideEffects } from '@ngrx/effects';
-import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
@@ -17,10 +10,7 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withComponentInputBinding()),
     provideHttpClient(),
-    provideStore(),
-    provideEffects(),
-    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
     provideAnimations(),
-    provideToastr({ positionClass: 'toast-bottom-right' }),
-  ],
+    provideToastr({ positionClass: 'toast-bottom-right' })
+  ]
 };
