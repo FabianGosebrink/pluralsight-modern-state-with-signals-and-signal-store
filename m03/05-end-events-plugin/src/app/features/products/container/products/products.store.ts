056946d2bc2a20f49a0bef4816cd5674116b2375
FabianGosebrink <fabian.gosebrink@outlook.com>
2025-12-17 21:09:23 +0100

    Refactor ProductsStore: clean up imports, enhance readability, and streamline method definitions for improved maintainability.

--- m03/05-end-events-plugin/src/app/features/products/container/products/products.store.ts
+++ m03/05-end-events-plugin/src/app/features/products/container/products/products.store.ts
@@ -1,13 +1,24 @@
 import { computed, inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { patchState, signalMethod, signalStore, withComputed, withHooks, withMethods, withState } from '@ngrx/signals';
-import { CATEGORY_NAME_MAP, Product, ProductCategory } from '../../../../shared/models/product.models';
+import {
+  patchState,
+  signalStore,
+  withComputed,
+  withHooks,
+  withMethods,
+  withState,
+} from '@ngrx/signals';
+import {
+  CATEGORY_NAME_MAP,
+  Product,
+  ProductCategory,
+} from '../../../../shared/models/product.models';
 import { GlobalCheckoutStore } from '../../../../shared/store/global-checkout.store';
 import { GlobalProductsStore } from '../../../../shared/store/global-products.store';
 
 export const ProductsStore = signalStore(
   withState({
-    searchTerm: ''
+    searchTerm: '',
   }),
   withComputed((_store, globalProductsStore = inject(GlobalProductsStore)) => ({
     loading: globalProductsStore.loading,
@@ -22,41 +33,37 @@ export const ProductsStore = signalStore(
 
           return result;
         },
-        {}
+        {},
       );
 
       const categories = Object.keys(productsByCategory);
 
       return categories.map((category) => ({
         category: CATEGORY_NAME_MAP[category as ProductCategory],
-        products: productsByCategory[category]
+        products: productsByCategory[category],
       }));
-    })
+    }),
   })),
   withMethods(
     (
       store,
       globalCheckoutStore = inject(GlobalCheckoutStore),
       globalProductsStore = inject(GlobalProductsStore),
-      router = inject(Router)
+      router = inject(Router),
     ) => ({
-      addToCart(product: Product) {
-        globalCheckoutStore.addToCart(product);
-      },
-      loadByQuery: signalMethod<string>((query) => {
-        globalProductsStore.loadByQuery(query);
-      }),
+      addToCart: globalCheckoutStore.addToCart,
+      loadByQuery: globalProductsStore.loadByQuery,
       onProductClicked(id: string): void {
         router.navigate(['products', id]);
       },
       searchValueChanged(searchTerm: string) {
         patchState(store, { searchTerm });
-      }
-    })
+      },
+    }),
   ),
   withHooks({
     onInit(store, globalProductsStore = inject(GlobalProductsStore)) {
       globalProductsStore.loadByQuery(store.searchTerm());
-    }
-  })
+    },
+  }),
 );
