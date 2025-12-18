357d40a19917e73e5cebb334e4994146c5865cd5
Fabian Gosebrink <fabian.gosebrink@outlook.com>
2025-12-18 10:26:27 +0100

    Add unit test for `cartProductsCount` signal in `ShellComponent`

--- m03/06-end-testing-2/src/app/features/shell/container/shell/shell.component.spec.ts
+++ m03/06-end-testing-2/src/app/features/shell/container/shell/shell.component.spec.ts
@@ -1,14 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideRouter } from '@angular/router';
+import { signal } from '@angular/core';
 import { ShellComponent } from './shell.component';
+import { ShellStore } from './shell.store';
 
 describe('ShellComponent', () => {
   let component: ShellComponent;
   let fixture: ComponentFixture<ShellComponent>;
 
+  const mockShellStore = {
+    cartProductsCount: signal(0),
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ShellComponent],
+      providers: [
+        provideRouter([]),
+        {
+          provide: ShellStore,
+          useValue: mockShellStore,
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ShellComponent);
@@ -18,5 +31,9 @@ describe('ShellComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have cartProductsCount signal', () => {
+    expect(component.store.cartProductsCount()).toBe(0);
   });
 });
