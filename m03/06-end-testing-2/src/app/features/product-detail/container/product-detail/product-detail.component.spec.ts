0a57ef7d04bddcc49ec323c7e60df57e0e8d37f2
Fabian Gosebrink <fabian.gosebrink@outlook.com>
2025-12-18 10:28:50 +0100

    Mock `ProductDetailStore` in unit tests for `ProductDetailComponent`

--- m03/06-end-testing-2/src/app/features/product-detail/container/product-detail/product-detail.component.spec.ts
+++ m03/06-end-testing-2/src/app/features/product-detail/container/product-detail/product-detail.component.spec.ts
@@ -1,17 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { ProductDetailComponent } from './product-detail.component';
-import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
+import { signal } from '@angular/core';
+import { ProductDetailComponent } from './product-detail.component';
+import { ProductDetailStore } from './product-detail.store';
 
 describe('ProductDetailComponent', () => {
   let component: ProductDetailComponent;
   let fixture: ComponentFixture<ProductDetailComponent>;
 
+  const mockLoadProductIfNotLoaded = jasmine.createSpy(
+    'loadProductIfNotLoaded',
+  );
+
+  const mockProductDetailStore = {
+    productDetail: signal(null),
+    loading: signal(false),
+    addToCart: jasmine.createSpy('addToCart'),
+    loadProductIfNotLoaded: mockLoadProductIfNotLoaded,
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ProductDetailComponent],
-      providers: [provideHttpClient(), provideRouter([])],
+      providers: [
+        provideRouter([]),
+        {
+          provide: ProductDetailStore,
+          useValue: mockProductDetailStore,
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ProductDetailComponent);
