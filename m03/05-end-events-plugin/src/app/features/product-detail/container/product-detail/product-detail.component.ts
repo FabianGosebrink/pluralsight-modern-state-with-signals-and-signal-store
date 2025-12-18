98ef11eaf61b77088e305ce0ce7ec24446708c6e
Fabian Gosebrink <fabian.gosebrink@outlook.com>
2025-12-18 14:53:10 +0100

    Refactor `ProductDetailStore` to use `@ngrx/signals/events` for streamlined state management and update `ProductDetailComponent` to dispatch events.

--- m03/05-end-events-plugin/src/app/features/product-detail/container/product-detail/product-detail.component.ts
+++ m03/05-end-events-plugin/src/app/features/product-detail/container/product-detail/product-detail.component.ts
@@ -2,20 +2,25 @@ import { Component, inject, input, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ProductImageComponent } from '../../presentational/product-image/product-image.component';
 import { ProductInfoComponent } from '../../presentational/product-info/product-info.component';
-import { ProductDetailStore } from './product-detail.store';
+import {
+  productDetailPageEvents,
+  ProductDetailStore,
+} from './product-detail.store';
+import { injectDispatch } from '@ngrx/signals/events';
 
 @Component({
   selector: 'app-product-detail',
   imports: [RouterLink, ProductImageComponent, ProductInfoComponent],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.scss',
-  providers: [ProductDetailStore]
+  providers: [ProductDetailStore],
 })
 export class ProductDetailComponent implements OnInit {
   store = inject(ProductDetailStore);
+  dispatch = injectDispatch(productDetailPageEvents);
   id = input.required<string>();
 
   ngOnInit() {
-    this.store.loadProductIfNotLoaded(this.id);
+    this.dispatch.opened(this.id());
   }
 }
--- m03/05-end-events-plugin/src/app/features/product-detail/container/product-detail/product-detail.store.ts
+++ m03/05-end-events-plugin/src/app/features/product-detail/container/product-detail/product-detail.store.ts
@@ -1,53 +1,120 @@
-import { patchState, signalStore, withComputed, withMethods, withState } from '@ngrx/signals';
+import { signalStore, type, withComputed, withState } from '@ngrx/signals';
 import { computed, inject } from '@angular/core';
 import { GlobalProductsStore } from '../../../../shared/store/global-products.store';
 import { GlobalCheckoutStore } from '../../../../shared/store/global-checkout.store';
-import { rxMethod } from '@ngrx/signals/rxjs-interop';
-import { exhaustMap, filter, pipe, tap } from 'rxjs';
-import { tapResponse } from '@ngrx/operators';
+import { exhaustMap, filter, map, tap } from 'rxjs';
+import {
+  eventGroup,
+  Events,
+  on,
+  withEffects,
+  withReducer,
+} from '@ngrx/signals/events';
 import { ProductDetailService } from '../../service/product-detail.service';
-import { setLoading, setLoadingFinished, withLoadingFeature } from '../../../../shared/store/loading-feature';
+import {
+  setLoading,
+  setLoadingFinished,
+  withLoadingFeature,
+} from '../../../../shared/store/loading-feature';
+import { Product } from '../../../../shared/models/product.models';
+import { mapResponse } from '@ngrx/operators';
+
+export const productDetailPageEvents = eventGroup({
+  source: 'Product Detail Page',
+  events: {
+    opened: type<string>(),
+    addToCartClicked: type<Product>(),
+  },
+});
+
+export const productDetailApiEvents = eventGroup({
+  source: 'Product Detail API',
+  events: {
+    loadProduct: type<string>(),
+    productLoadedSuccess: type<Product>(),
+    productLoadedFailure: type<unknown>(),
+  },
+});
 
 export const ProductDetailStore = signalStore(
-  withLoadingFeature(),
   withState({
-    productId: null as string | null
+    productId: null as string | null,
   }),
+
+  withLoadingFeature(),
+
+  withReducer(
+    on(productDetailPageEvents.opened, ({ payload }) => [
+      { productId: payload },
+    ]),
+
+    on(productDetailApiEvents.loadProduct, () => setLoading()),
+
+    on(
+      productDetailApiEvents.productLoadedSuccess,
+      productDetailApiEvents.productLoadedFailure,
+      () => setLoadingFinished(),
+    ),
+  ),
+
   withComputed((store, globalProductsStore = inject(GlobalProductsStore)) => ({
     productDetail: computed(() => {
       const productId = store.productId() ?? '';
+      console.log('productDetail', productId);
       const existingProduct = globalProductsStore.entityMap()[productId];
-
       return existingProduct ?? null;
-    })
+    }),
   })),
-  withMethods(
+
+  withEffects(
     (
-      store,
+      _store,
+      events = inject(Events),
       globalCheckoutStore = inject(GlobalCheckoutStore),
       globalProductsStore = inject(GlobalProductsStore),
-      productDetailService = inject(ProductDetailService)
+      productDetailService = inject(ProductDetailService),
     ) => ({
-      addToCart: globalCheckoutStore.addToCart,
-      loadProductIfNotLoaded: rxMethod<string>(
-        pipe(
-          tap((productId) => patchState(store, { productId })),
-          filter((productId) => !globalProductsStore.entityMap()[productId]),
-          tap(() => patchState(store, setLoading())),
-          exhaustMap((id) =>
-            productDetailService.loadProductDetail(id).pipe(
-              tapResponse({
-                next: (product) => {
-                  patchState(store, setLoadingFinished());
+      loadProductIfNotAlreadyPresent$: events
+        .on(productDetailPageEvents.opened)
+        .pipe(
+          filter(({ payload }) => !globalProductsStore.entityMap()[payload]),
+          map(({ payload }) => productDetailApiEvents.loadProduct(payload)),
+        ),
 
-                  globalProductsStore.add(product);
-                },
-                error: console.error
-              })
-            )
-          )
-        )
-      )
-    })
-  )
+      loadProduct$: events.on(productDetailApiEvents.loadProduct).pipe(
+        exhaustMap(({ payload }) =>
+          productDetailService.loadProductDetail(payload).pipe(
+            mapResponse({
+              next: (product) =>
+                productDetailApiEvents.productLoadedSuccess(product),
+              error: (error: unknown) =>
+                productDetailApiEvents.productLoadedFailure(error),
+            }),
+          ),
+        ),
+      ),
+
+      addToGlobalStore$: events
+        .on(productDetailApiEvents.productLoadedSuccess)
+        .pipe(
+          tap(({ payload }) => {
+            globalProductsStore.add(payload);
+          }),
+        ),
+
+      addToCart$: events.on(productDetailPageEvents.addToCartClicked).pipe(
+        tap(({ payload }) => {
+          globalCheckoutStore.addToCart(payload);
+        }),
+      ),
+
+      logErrors$: events
+        .on(productDetailApiEvents.productLoadedFailure)
+        .pipe(
+          tap(({ payload }) =>
+            console.error(`Failed to load product`, payload),
+          ),
+        ),
+    }),
+  ),
 );
